5cd8109540db5e95c61ffac2abdb089d27f261c9
Andrew Pouliot <andrew@darknoon.com>
2020-08-01 18:01:48 -0400

    Initial Commit

---
+++ MultiRAW/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MultiRAW
+//
+//  Created by Andrew Pouliot on 8/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiRAW/MultiRAWApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiRAWApp.swift
+//  MultiRAW
+//
+//  Created by Andrew Pouliot on 8/1/20.
+//
+
+import SwiftUI
+
+@main
+struct MultiRAWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
